e45df2cdd782a6543a83323a5f94ea2c40376d13
karizune <luanchrystian2@gmail.com>
2022-08-03 01:07:19 -0300

    adicionando environments

---
+++ dozedex/src/environments/environments.ts
@@ -0,0 +1,18 @@
+const API_URL: string = "https://dozedex-api.herokuapp.com";
+const LOCAL_URL: string = "localhost:3333";
+
+//altere aqui para dinamicamente pegar url local ou hospedada
+const isLocal = false;
+
+function useLocal(use: Boolean): string{
+    if(!use){
+        return API_URL;
+    }
+    else{
+        return LOCAL_URL;
+    }
+}
+
+export const environment = {
+    API_URL: useLocal(isLocal)
+};
